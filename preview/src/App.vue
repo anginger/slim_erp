0aec41247219da34ecb9ed17055910dafab63b1b
SuperSonic <supersonic@livemail.tw>
2021-12-31 21:18:19 +0800

    Update

--- preview/src/App.vue
+++ preview/src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <v-app id="inspire">
-    <loading v-if="!$store.state.loaded"/>
-    <login v-else-if="!$store.state.ready" @success="loginSuccess"/>
+    <disconnected v-if="disconnected" />
+    <loading v-else-if="!$store.state.loaded" />
+    <login v-else-if="!$store.state.ready" @success="loginSuccess" />
     <div v-else>
-      <app-bar/>
+      <app-bar />
       <v-navigation-drawer app>
-        <nav-drawer/>
-        <drawer/>
+        <nav-drawer />
+        <drawer />
       </v-navigation-drawer>
       <v-main>
-        <router-view/>
+        <router-view />
       </v-main>
-      <app-footer/>
+      <app-footer />
     </div>
   </v-app>
 </template>
@@ -23,29 +24,43 @@ import AppFooter from "./components/Layout/AppFooter";
 import Login from "./components/Login";
 import Loading from "./components/Loading";
 import Drawer from "./components/Layout/Drawer";
+import Disconnected from "./components/Disconnected.vue";
 
 export default {
-  components: {Drawer, Loading, Login, AppFooter, NavDrawer, AppBar},
+  name: "Slim",
+  components: {
+    Drawer,
+    Loading,
+    Login,
+    AppFooter,
+    NavDrawer,
+    AppBar,
+    Disconnected,
+  },
+  data: () => ({
+    disconnected: true,
+  }),
   methods: {
     async checkStatus() {
       try {
-        const response = await this.$axios.get("/authentic/session")
-        this.$store.commit("setReady", response.status === 200)
-        this.$store.commit("setUser", response.data)
+        const response = await this.$axios.get("/authentic/session");
+        this.disconnected = response.status === 503;
+        this.$store.commit("setReady", response.status === 200);
+        this.$store.commit("setUser", response.data);
       } catch (e) {
-        console.warn(e)
+        console.warn(e);
       }
       setTimeout(() => {
-        this.$store.commit("setLoaded", true)
-      }, 3000)
+        this.$store.commit("setLoaded", true);
+      }, 3000);
     },
     loginSuccess() {
-      this.$store.commit("setLoaded", false)
-      this.checkStatus()
-    }
+      this.$store.commit("setLoaded", false);
+      this.checkStatus();
+    },
   },
   created() {
-    this.checkStatus()
-  }
-}
+    this.checkStatus();
+  },
+};
 </script>
---
+++ preview/src/components/Disconnected.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-container>
+    <v-card flat>
+      <v-card-title>
+        <v-avatar color="grey lighten-5">
+          <v-img
+              max-width="15"
+              max-height="35"
+              :src="require('../assets/logo.png')"
+          />
+        </v-avatar>
+        <v-card-title>
+          Slim ERP Framework
+        </v-card-title>
+      </v-card-title>
+    </v-card>
+    <v-card outlined rounded>
+      <v-card-title>
+        Slim 與後端伺服器通訊失敗，伺服器可能尚未設定、網路失效，或遇到未知錯誤，請稍後再試。
+      </v-card-title>
+      <v-card-subtitle>
+        Slim failed to connect with the backend server. The server may not be configured, the network is down, or an unknown error has been encountered. Please try again later.
+      </v-card-subtitle>
+      <v-card-subtitle>
+        &copy; Slim 版權為
+        <a class="text-decoration-none" href="https://starinc.xyz">Star Inc.</a>
+        所有，以 MIT 條款開放原始碼授權。
+      </v-card-subtitle>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Disconnected"
+}
+</script>

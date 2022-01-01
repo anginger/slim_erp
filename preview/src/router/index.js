de4e6e44cb02676f03586cacb4428932064f35e7
SuperSonic <supersonic@livemail.tw>
2022-01-01 14:27:14 +0800

    Fix bugs

--- preview/src/router/index.js
+++ preview/src/router/index.js
@@ -1,27 +1,27 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import VueRouter from "vue-router";
 import manage from "./manage";
 import Manage from "../views/Manage";
 import Logout from "@/views/Logout";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/manage',
-    name: 'Manage',
+    path: "/manage",
+    name: "Manage",
     component: Manage,
     children: manage
   },
   {
-      path: '/logout',
-      name: 'Logout',
-      component: Logout
+    path: "/logout",
+    name: "Logout",
+    component: Logout
   }
-]
+];
 
 const router = new VueRouter({
   routes
-})
+});
 
-export default router
+export default router;
--- preview/src/router/manage.js
+++ preview/src/router/manage.js
@@ -1,33 +1,33 @@
-import Overview from '@/views/Manage/Overview.vue'
+import Overview from "@/views/Manage/Overview.vue";
 import Users from "@/views/Manage/Users";
 import Products from "@/views/Manage/Products";
 import Provider from "@/views/Manage/Providers";
 import Levels from "@/views/Manage/Levels";
 
 export default [
-    {
-        path: '',
-        name: 'Overview',
-        component: Overview
-    },
-    {
-        path: 'users',
-        name: 'Users',
-        component: Users
-    },
-    {
-        path: 'products',
-        name: 'Products',
-        component: Products
-    },
-    {
-        path: 'providers',
-        name: 'Providers',
-        component: Provider
-    },
-    {
-        path: 'levels',
-        name: 'Levels',
-        component: Levels
-    }
-]
+  {
+    path: "",
+    name: "Overview",
+    component: Overview
+  },
+  {
+    path: "users",
+    name: "Users",
+    component: Users
+  },
+  {
+    path: "products",
+    name: "Products",
+    component: Products
+  },
+  {
+    path: "providers",
+    name: "Providers",
+    component: Provider
+  },
+  {
+    path: "levels",
+    name: "Levels",
+    component: Levels
+  }
+];
